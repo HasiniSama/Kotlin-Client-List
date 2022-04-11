ebd6bbe33c86abc295b4d645983de78717d36ade
Hasini <hasinisama99@gmail.com>
2022-04-11 08:01:25 +0530

    Added recyclerview

--- app/src/main/java/com/kln/android/myapplication/ClientListFragment.kt
+++ app/src/main/java/com/kln/android/myapplication/ClientListFragment.kt
@@ -6,7 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.kln.android.myapplication.adapter.ClientListAdapter
 import com.kln.android.myapplication.databinding.FragmentFirstBinding
+import com.kln.android.myapplication.model.ClientsViewModel
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -25,6 +29,28 @@ class ClientListFragment : Fragment() {
     ): View? {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        // getting the recyclerview by its id
+        val recyclerview = binding.recyclerview
+
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(activity)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ClientsViewModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(ClientsViewModel(R.drawable.ic_baseline_account_circle_24, "Item " + i, 1.2f, 2.3f))
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = ClientListAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        recyclerview.adapter = adapter
+
         return binding.root
 
     }
@@ -32,9 +58,9 @@ class ClientListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+//        binding.buttonFirst.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/kln/android/myapplication/model/ClientsViewModel.kt
@@ -0,0 +1,3 @@
+package com.kln.android.myapplication.model
+
+data class ClientsViewModel(val image: Int, val name: String, val lat: Float, val long: Float)
